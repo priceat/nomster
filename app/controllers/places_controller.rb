17cf8b33e152ad7d4d1a79fc51492f0d66b9f161
Tate Price <priceat@gmail.com>
2015-02-06 20:25:52 +0000

    Finish Update and Destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,31 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Things change.. As do places. Well done!" }
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Place was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def place_params
