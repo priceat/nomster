eb6b09f3cbb0ab5a9aae11b1a60c7bb9414483eb
Tate Price <priceat@gmail.com>
2015-02-11 17:27:34 +0000

    basic email functionality

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ActionMailer::Base
+    default from: "no-reply@nomsterapp.com"
+
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "New comment on #{@place.name}")
+  end
+end
